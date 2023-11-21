d3431ecbb0e26d2b39c32c2b80da36c3b2801c41
ana-adamovic <adamovicana02@gmail.com>
2023-11-21 19:38:30 +0100

    Ubacen podmodul zajednicki

---
+++ zajednicki/src/main/java/rs/np/planovi/zajednicki/transfer/util/ResponseStatus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.np.planovi.zajednicki.transfer.util;
+
+/**
+ *
+ * @author adamo
+ */
+public enum ResponseStatus {
+    Success, Error
+}
